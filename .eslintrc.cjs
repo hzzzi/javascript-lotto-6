13743c9c420decf49dfca5166455324abdb80cd3
hzzzi <hji2004@naver.com>
2023-12-03 18:25:44 +0900

    style: eslint, prettier 설정

---
+++ .eslintrc.cjs
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: ['airbnb-base', 'prettier'],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'max-depth': ['error', 2],
+    'max-params': ['error', 3],
+    'max-lines-per-function': ['error', { max: 15, Ignores: ['**/*.test.js'] }],
+    'import/prefer-default-export': 'off',
+    'import/extensions': ['off'],
+  },
+};
