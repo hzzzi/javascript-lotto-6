7defde460760ff338917773f7da516ccfc5a49b8
hzzzi <hji2004@naver.com>
2023-12-03 21:28:04 +0900

    feat(Constants): 입출력 메세지, 게임 조건, 에러 메제시 상수 생성

--- src/Constants.js
+++ src/Constants.js
@@ -6,6 +6,36 @@ const GAME_MSG = {
     WINNING_NUMBER: '당첨 번호를 입력해 주세요.',
     BONUS_NUMBER: '보너스 번호를 입력해 주세요.',
   },
+  OUTPUT: {
+    RESULT: '당첨 통계',
+  },
 };
 
-export { LOTTO, NEW_LINE, GAME_MSG };
+const CONDITION = {
+  NUMBER: {
+    COUNT: 6,
+    MIN_RANGE: 1,
+    MAX_RANGE: 45,
+  },
+  PRICE: 1000,
+};
+
+const DEFAULT_ERROR = '[ERROR]';
+
+const ERROR_MSG = {
+  NOTHING: `${DEFAULT_ERROR} 값을 입력해주세요.`,
+  MINUS_NUMBER: `${DEFAULT_ERROR} 1 이상의 수만 입력해주세요.`,
+  NUMBER_NOT_SIX: `${DEFAULT_ERROR} 로또 번호는 6개여야 합니다.`,
+  NOT_IN_RANGE: `${DEFAULT_ERROR} 1부터 45 사이의 수를 입력해주세요.`,
+  PURCHASE_PRICE: {
+    NOT_DIVIDED_BY_PRICE: `${DEFAULT_ERROR} ${LOTTO.PRICE}원 단위로 입력해주세요.`,
+  },
+  WINNING_NUMBER: {
+    DUPLICATED: `${DEFAULT_ERROR} 중복된 수를 입력할 수 없습니다.`,
+  },
+  BONUS_NUMBER: {
+    DUPLICATED: `${DEFAULT_ERROR} 당첨 번호와 중복된 수를 입력할 수 없습니다.`,
+  },
+};
+
+export { NEW_LINE, GAME_MSG, CONDITION, ERROR_MSG };
---
+++ src/validator/PurchasePrice.js
@@ -0,0 +1,25 @@
+import { ERROR_MSG } from '../Constants';
+
+const PurchasePriceValidator = {
+  isNothing(input) {
+    if (input === '') {
+      throw new Error(ERROR_MSG.NOTHING);
+    }
+  },
+  isPositive(input) {
+    if (Number(input) <= 0) {
+      throw new Error(ERROR_MSG.MINUS_NUMBER);
+    }
+  },
+  isDividedByPrice(input) {
+    if (input % 1000 !== 0) {
+      throw new Error(ERROR_MSG.NOT_DIVIDED_BY_PRICE);
+    }
+  },
+
+  validatePurchasePrice(input) {
+    this.isNothing(input);
+    this.isPositive(input);
+    this.isDividedByPrice(input);
+  },
+};
